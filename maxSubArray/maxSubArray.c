6aac3e907c76c9bcdfb0ce066b3be74cff1d1689
westtrain <it.joshualee@gmail.com>
2020-04-03 19:19:04 -0700

    Largest Sum Contiguous Subarray

---
+++ maxSubArray/maxSubArray.c
@@ -0,0 +1,29 @@
+/*
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * Example:
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ * Follow up:
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ * Also you can use Kadane’s Algorithm for solve it.
+ */
+
+int maxSubArray(int* nums, int numsSizei)
+{
+    int i = 0;
+    int sum = 0;
+    int largeSum = nums[0];
+    
+    while (i < numsSize)
+    {
+        sum += nums[i];
+        if (largeSum < sum)
+            largeSum = sum;
+        if (sum < 0)
+            sum = 0;
+        i++;
+    }
+    return largeSum;
+}
