1d950a5506f3781f3a145b0d40a884e40932b615
westtrain <it.joshualee@gmail.com>
2020-04-05 14:59:06 -0700

    Single Number

--- singleNumber/singleNumber.c
+++ singleNumber/singleNumber.c
@@ -1,18 +1,31 @@
 /*
  * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ * Input: [2,2,1]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ * Runtime: 8 ms
+ * Memory Usage: 6.7 MB
  */
 
  int singleNumber(int* nums, int numsSize){
 
     int i = 0;
     int res = 0;
-    if (!nums)
-      return 0;
     while (i < numsSize)
     {
         res = res ^ nums[i];
         i++;
+        // too slow code below
+        // Runtime: 16 ms
         // while (j < numsSize)
         // {
         //     if (nums[i] == nums[j] && i != j)
