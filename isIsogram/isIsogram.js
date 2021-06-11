fd4c8576387b28ed4d68ef5a3907a4a84cc770b9
westtrain <it.joshualee@gmail.com>
2021-06-11 09:09:34 +0900

    isIsogram

---
+++ isIsogram/isIsogram.js
@@ -0,0 +1,10 @@
+function isIsogram(str) {
+  for(let i = 0; i < str.length; i++){
+    for(let j = i + 1; j < str.length; j++){
+      if(str[i].toLowerCase() === str[j].toLowerCase()){
+        return false;
+      }
+    }
+  }
+  return true;
+}
